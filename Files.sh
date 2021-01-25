64cf6e669df4d99ba38778a432ba705d2a09cf13
ZeCosta <josepedroc.costa@gmail.com>
2021-01-25 19:08:32 +0000

    cenas

--- Files.sh
+++ Files.sh
@@ -1,4 +1,3 @@
-subl src/Servidor/SimpleServer*.java
-subl src/Cliente/*Simple*.java
-subl src/Cliente/Stub.java
-subl src/A*/A*.java
+subl src/Servidor/*.java
+subl src/Cliente/*.java
+subl src/A*/*.java
