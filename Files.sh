a2af9fdbc8c8788086bddaee5cdfd0f64838e6a3
ZeCosta <josepedroc.costa@gmail.com>
2021-01-25 16:29:44 +0000

    mais ficheiros .sh

---
+++ Files.sh
@@ -0,0 +1,4 @@
+subl src/Servidor/SimpleServer*.java
+subl src/Cliente/*Simple*.java
+subl src/Cliente/Stub.java
+subl src/A*/A*.java
---
+++ Git.sh
@@ -0,0 +1,12 @@
+git add src/Cliente/*.java
+git add src/Servidor/Simple*.java
+git add src/AlarmeCovidLN/*.java
+
+if [ $# -eq 0 ]
+	then
+		git commit -m "cenas"
+	else
+		git commit -m "$1"
+fi
+
+git push
