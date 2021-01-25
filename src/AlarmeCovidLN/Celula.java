e685844536cc3fee8d2fc6fc125dcdc42441e109
111Bond <joaoprp111@gmail.com>
2021-01-25 19:50:10 +0000

    comunicar localizacao atualizada

--- src/AlarmeCovidLN/Celula.java
+++ src/AlarmeCovidLN/Celula.java
@@ -3,20 +3,30 @@ package src.AlarmeCovidLN;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.TreeSet;
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Celula {
     private Collection<String> users;
+    private int nPessoasAtual;
     private Lock l;
+    private Condition c;
 
     public Celula(){
         this.users = new TreeSet<>();
         this.l = new ReentrantLock();
+        this.c = l.newCondition();
+        this.nPessoasAtual = 0;
     }
 
     public void addUser(String user){
         users.add(user);
+        nPessoasAtual++;
+    }
+
+    public void reduzirN(){
+        nPessoasAtual--;
     }
 
     public void addAllUsers(Collection<String> users){
